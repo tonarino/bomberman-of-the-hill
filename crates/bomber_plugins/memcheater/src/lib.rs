f5dc68017b79249b5400f322f1e423439c7f1c63
Pablo Mansanet <pablomansanet@gmail.com>
2022-06-22 16:35:10 +0900

    Add memory cheater example

---
+++ crates/bomber_plugins/memcheater/src/lib.rs
@@ -0,0 +1,35 @@
+use bomber_lib::{
+    self,
+    world::{Object, Tile},
+    Action, LastTurnResult, Player,
+};
+use bomber_macro::wasm_export;
+
+#[derive(Default)]
+struct MemCheater;
+
+#[wasm_export]
+impl Player for MemCheater {
+    #[allow(clippy::empty_loop)]
+    fn act(
+        &mut self,
+        _surroundings: Vec<(Tile, Option<Object>, bomber_lib::world::TileOffset)>,
+        _last_result: LastTurnResult,
+    ) -> Action {
+        // Look at all this memory!
+        let big_vec = vec![0u32; 500_000_000];
+        if big_vec.len() > 5000000 {
+            Action::StayStill
+        } else {
+            Action::DropBomb
+        }
+    }
+
+    fn name(&self) -> String {
+        "Big Brain".into()
+    }
+
+    fn team_name() -> String {
+        "Move things break fast".into()
+    }
+}
