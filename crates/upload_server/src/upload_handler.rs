a7547986e661798d768dd7f04c782a00f92f4076
Matěj Laitl <matej@laitl.cz>
2022-07-07 11:01:12 +0200

    upload server: log what we respond to clients
    Otherwise the logs are pretty opaque. Need to use a non-elegant way because rouille::Response is
    pretty limited (cannot read the text response without consuming it).

--- crates/upload_server/src/upload_handler.rs
+++ crates/upload_server/src/upload_handler.rs
@@ -16,19 +16,19 @@ const INTERNAL_SERVER_ERROR: u16 = 500;
 
 pub fn handler(request: &Request, api_keys: &[String]) -> Response {
     if request.method() != "POST" {
-        return Response::text("We only accept HTTP POST.\n").with_status_code(METHOD_NOT_ALLOWED);
+        return text_response("We only accept HTTP POST.\n").with_status_code(METHOD_NOT_ALLOWED);
     }
 
     let api_key = match request.header("Api-Key") {
         Some(api_key) => {
             if api_keys.iter().all(|allowed_key| allowed_key != api_key) {
-                return Response::text(format!("HTTP header Api-Key \"{}\" not valid.\n", api_key))
+                return text_response(format!("HTTP header Api-Key \"{}\" not valid.\n", api_key))
                     .with_status_code(UNAUTHORIZED);
             }
             api_key
         },
         None => {
-            return Response::text("HTTP header Api-Key not present, please include it.")
+            return text_response("HTTP header Api-Key not present, please include it.")
                 .with_status_code(UNAUTHORIZED)
         },
     };
@@ -36,24 +36,23 @@ pub fn handler(request: &Request, api_keys: &[String]) -> Response {
     if let Some(mut body) = request.data() {
         let mut data = Vec::new();
         if let Err(e) = body.read_to_end(&mut data) {
-            return Response::text(format!("Failed to read input body: {}\n", e))
+            return text_response(format!("Failed to read input body: {}\n", e))
                 .with_status_code(INTERNAL_SERVER_ERROR);
         }
         if data.len() > MAX_WASM_SIZE {
-            return Response::text(format!("Maximum size of {} exceeded.\n", MAX_WASM_SIZE))
+            return text_response(format!("Maximum size of {} exceeded.\n", MAX_WASM_SIZE))
                 .with_status_code(BAD_REQUEST);
         }
         if !data.starts_with(WASM_FILE_PREFIX) {
-            return Response::text("Uploaded data not a WASM file.\n")
-                .with_status_code(BAD_REQUEST);
+            return text_response("Uploaded data not a WASM file.\n").with_status_code(BAD_REQUEST);
         }
         match handle_upload(api_key, &data) {
-            Ok(()) => Response::text("Your submission has been accepted.\n"),
-            Err(e) => Response::text(format!("Error accepting your submission: {:#}\n", e))
+            Ok(()) => text_response("Your submission has been accepted.\n"),
+            Err(e) => text_response(format!("Error accepting your submission: {:#}\n", e))
                 .with_status_code(INTERNAL_SERVER_ERROR),
         }
     } else {
-        Response::text("Please submit request with body.\n").with_status_code(BAD_REQUEST)
+        text_response("Please submit request with body.\n").with_status_code(BAD_REQUEST)
     }
 }
 
@@ -68,4 +67,12 @@ fn handle_upload(api_key: &str, data: &[u8]) -> Result<(), Error> {
     fs::rename(&temp_path, &path)?;
     info!("{:?} saved.", path);
     Ok(())
+}
+
+/// Create a text response and log it. Work-around for the fact that response body can be read only
+/// once from [rouille::Response]. Use instead of text_response(...).
+fn text_response(text: impl Into<String>) -> Response {
+    let text: String = text.into();
+    debug!("Responding with: {}", text.trim());
+    Response::text(text)
 }
