3c0151257d41bd9083a75a457ff3d6c266095b48
PabloMansanet <pablomansanet@gmail.com>
2022-07-08 14:10:54 +0900

    decide the first round

--- crates/bomber_game/src/state.rs
+++ crates/bomber_game/src/state.rs
@@ -2,7 +2,7 @@
 
 use anyhow::Result;
 use bevy::prelude::*;
-use std::time::Duration;
+use std::{path::Path, time::Duration};
 
 use crate::log_unrecoverable_error_and_panic;
 
@@ -21,14 +21,25 @@ pub struct Round(pub u32);
 
 const GAME_DURATION: Duration = Duration::from_secs(5 * 60);
 const VICTORY_SCREEN_DURATION: Duration = Duration::from_secs(30);
+const FINISHED_ROUND_MARKER_FILENAME: &str = "round-finished.marker";
+const ROUNDS_FOLDER: &str = "rounds";
+const MAX_ROUNDS: u32 = 10_000;
 
 #[derive(Component)]
 pub struct RoundTimer(pub Timer);
 
 impl Plugin for AppStatePlugin {
     fn build(&self, app: &mut App) {
+        let first_round = (1..MAX_ROUNDS)
+            .find(|r| {
+                let rounds_path = Path::new(ROUNDS_FOLDER);
+                let round_path = rounds_path.join(r.to_string());
+                !round_path.join(FINISHED_ROUND_MARKER_FILENAME).exists()
+            })
+            .expect("All possible round slots are full");
+
         app.add_startup_system(setup)
-            .insert_resource(Round(1))
+            .insert_resource(Round(first_round))
             .add_system(app_state_system.chain(log_unrecoverable_error_and_panic))
             .add_state(AppState::InGame);
     }
